37d8337d1c654ef36d19b6ca128f30f9bebe21c3
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2024-05-09 20:55:21 -0500

    a

--- user/a_docum/provision.py
+++ user/a_docum/provision.py
@@ -28,10 +28,6 @@ def provision_dfu_util():
     # STM32F4: works, error: "dfu-util: can't detach"
     dfu_util_package = "dfu-util-0.9-3-x86_64.pkg.tar.zst"
 
-    # LPC1768: TODO
-    # STM32F4: TODO
-    # dfu_util_package = "dfu-util-0.10-3-x86_64.pkg.tar.zst"
-
     dfu_util_url = f"https://archive.archlinux.org/packages/d/dfu-util/{dfu_util_package}"
     os.system(f"wget {dfu_util_url} --output-document={dfu_util_package} --no-clobber")
     os.system(f"sudo pacman --upgrade {dfu_util_package} --noconfirm --overwrite '*'")
